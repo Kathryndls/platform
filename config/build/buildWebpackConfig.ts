563390d3af3001ed84bd9ad322a0a058555ada91
katerinakolesnykova <kolesnykovakaterina@gmail.com>
2024-11-06 18:45:29 +0200

    ci id3 fixed

--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -1,9 +1,9 @@
+import webpack = require('webpack');
 import {BuildOptions} from './types/config';
 import {buildPlugins} from './buildPlugins';
 import {buildLoaders} from './buildLoaders';
 import {buildResolvers} from './buildResolvers';
 import {buildDevServer} from './buildDevServer';
-import webpack = require('webpack');
 
 export function buildWebpackConfig(options: BuildOptions): webpack.Configuration {
     const {paths, mode, isDev} = options;
