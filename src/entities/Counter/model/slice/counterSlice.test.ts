375778aed08400b703f80c9f2205924de490237c
katerinakolesnykova <kolesnykovakaterina@gmail.com>
2024-11-10 11:55:59 +0200

    add redux store, add first entity, add test and global style

--- src/entities/Counter/model/slice/counterSlice.test.ts
+++ src/entities/Counter/model/slice/counterSlice.test.ts
@@ -1,5 +1,5 @@
-import { counterReducer, counterActions } from './counterSlice';
 import { CounterSchema } from 'entities/Counter';
+import { counterReducer, counterActions } from './counterSlice';
 
 describe('counterSlice.test', () => {
     test('decrement', () => {
