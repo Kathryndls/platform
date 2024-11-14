b77f60eaa0373ece855f3ec0e17d1cd8d3924f69
katerinakolesnykova <kolesnykovakaterina@gmail.com>
2024-11-14 16:23:41 +0200

    fixed ci cd

--- src/entities/Counter/index.ts
+++ src/entities/Counter/index.ts
@@ -1,6 +1,6 @@
+import { Counter } from 'entities/Counter/ui/Counter';
 import { counterReducer } from './model/slice/counterSlice';
 import type { CounterSchema } from './model/types/counterSchema';
-import { Counter } from 'entities/Counter/ui/Counter';
 
 export {
     counterReducer,
