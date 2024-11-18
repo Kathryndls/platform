0c0dfca8641977a302edc2af542d7c25cbef4190
katerinakolesnykova <kolesnykovakaterina@gmail.com>
2024-11-18 18:42:18 +0200

    fixed ci cd

--- src/widgets/Sidebar/model/items.ts
+++ src/widgets/Sidebar/model/items.ts
@@ -1,8 +1,8 @@
 import React from 'react';
 import { RoutePath } from 'shared/config/routeConfig/routeConfig';
-import AboutIcon from ' shared/assets/icons/about.svg';
-import MainIcon from ' shared/assets/icons/main.svg';
-import ProfileIcon from ' shared/assets/icons/profile.svg';
+import AboutIcon from 'shared/assets/icons/about.svg';
+import MainIcon from 'shared/assets/icons/main.svg';
+import ProfileIcon from 'shared/assets/icons/profile.svg';
 
 export interface SidebarItemType {
     path: string;
