3221f0d76bf23ec2b05b95133f1eaec6943acedd
katerinakolesnykova <kolesnykovakaterina@gmail.com>
2024-11-28 00:41:05 +0200

    fix typings and use strict problem, add user form, fix tests

--- src/entities/Profile/model/slice/profileSlice.ts
+++ src/entities/Profile/model/slice/profileSlice.ts
@@ -1,6 +1,6 @@
 import {createSlice, PayloadAction} from '@reduxjs/toolkit';
-import {Profile, ProfileSchema} from '../types/profile';
 import {fetchProfileData} from 'entities/Profile';
+import {Profile, ProfileSchema} from '../types/profile';
 
 const initialState: ProfileSchema = {
     readonly: true,
--- src/features/AuthByForm/ui/LoginForm/LoginForm.stories.tsx
+++ src/features/AuthByForm/ui/LoginForm/LoginForm.stories.tsx
@@ -13,5 +13,5 @@ const Template: ComponentStory<typeof LoginForm> = (args) => <LoginForm {...args
 
 export const Primary = Template.bind({});
 Primary.args = {
-    className: 'example-class'
+    className: 'example-class',
 };
